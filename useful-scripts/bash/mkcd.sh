127628d0c10e2fbaacdcbe238d9230c46c1a8014
phunc20 <wucf20@gmail.com>
2020-10-03 18:44:37 +0700

    add mkcd

---
+++ useful-scripts/bash/mkcd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#mkcd() {
+#    [ "$#" -ne 1 ] && echo "Only one input arg allowed." && exit 1
+#    [ -z "$1" ] && echo "Please provide one input arg." && exit 1
+#    mkdir -p "$1"
+#    cd "$1"
+#}
+
+## The above seems to work badly because the `exit 1` makes the shell to close itself.
+
+mkcd() {
+    if [ "$#" -ne 1 ]; then
+        echo "Exactly one input arg required."
+    else
+        mkdir -p "$1"
+        cd "$1"
+    fi
+}
