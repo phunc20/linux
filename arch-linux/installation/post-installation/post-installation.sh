d6ae7eacfbc443d99f361fa897f8a201ecf0ddd0
phunc20 <wucf20@gmail.com>
2022-05-28 15:17:25 +0700

    add more pkgs to post_intsall pacman

--- arch-linux/installation/post-installation/post-installation.sh
+++ arch-linux/installation/post-installation/post-installation.sh
@@ -1,10 +1,20 @@
 sudo pacman -S \
-  terminus-font \ # tty bigger font
-  xorg-server xorg-xinit  # X
-  #git ssh-tools           # git
-  #xorg-xmodmap            # keyboard remap
-  #xorg-xrdb
-  #firefox                 # web browser
-  ##noto-font              # browser font
-  #dunst
-  #ibus                    # input method
+  terminus-font           `# tty bigger font` \
+  xorg-server xorg-xinit  `# X` \
+  git ssh-tools           `# git` \
+  xorg-xmodmap            `# keyboard remap` \
+  xorg-xrdb               \
+  xorg-xinput            `# to configure devices like trackpad, trackpoint, etc. E.g. cf. ~/.config/sxhkd/sxhkdrc` \
+  firefox                 `# web browser` \
+  noto-fonts              `# browser font` \
+  ttf-libertaion          `# browser font` \
+  dunst                   \
+  ibus ibus-unikey ibus-pinyin  `# input method` \
+  tree                    \
+  mdcat                   \
+  redshift                   \
+  zathura zathura-pdf-mupdf zathura-djvu zathura-ps  \
+  alsa-utils
+
+
+#vimplug
